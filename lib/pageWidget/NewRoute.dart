e13aa7ea5e75b9b45000ab59741360cefc533a49
15979275225 <15979275225@163.com>
2021-05-18 22:53:33 +0800

    新增第三方包

--- lib/pageWidget/NewRoute.dart
+++ lib/pageWidget/NewRoute.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 class NewRoute extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    final wordPair = new WordPair.random();
     return Scaffold(
       appBar: AppBar(
         title: Text("New route"),
       ),
       body: Center(
-        child: Text("this is new route"),
+        child: Text(wordPair.toString()),
       ),
     );
   }
