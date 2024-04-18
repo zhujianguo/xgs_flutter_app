4c29bc66870e0382f13b4524cd394cb7e7f6fee2
朱建国 <zhujg@akulaku.com>
2024-04-18 10:12:59 +0800

    调整tab2的页面样式

--- lib/pageWidget/tabs/ThirdPartyLibrariesPage.dart
+++ lib/pageWidget/tabs/ThirdPartyLibrariesPage.dart
@@ -4,42 +4,48 @@ class ThirdPartyLibrariesPage extends StatelessWidget {
   static const String routeName = "thirdPartyLibrariesPage";
 
   const ThirdPartyLibrariesPage({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-          children: [
-            _myWidget(context, 'textDemoPage', 'Text使用'),
-            _myWidget(context, 'buttonDemoPage', '按钮使用'),
-            _myWidget(context, 'imageDemoPage', 'Image使用'),
-            _myWidget(context, 'customPaintTest', 'customPaint自定义组件'),
-          ],
-        ));
+      children: [
+        _myWidget(context, 'textDemoPage', 'Text使用'),
+        _myWidget(context, 'buttonDemoPage', '按钮使用'),
+        _myWidget(context, 'imageDemoPage', 'Image使用'),
+        _myWidget(context, 'customPaintTest', 'customPaint自定义组件'),
+      ],
+    ));
   }
 
   Widget _myWidget(BuildContext context, String routerName, String textInfo) {
     return Container(
-      color: Colors.white70,
-      width: MediaQuery.of(context).size.width, // 获取屏幕宽
-      padding: const EdgeInsets.all(6.0), // 四个方向间隔6像素
-      margin: const EdgeInsets.only(bottom: 1), // 和下面的组件间隔1像素
-      child: Column(
-        children: [
-          GestureDetector(
-            child: Text(
-              textInfo,
-              style: const TextStyle(
-                fontWeight: FontWeight.w900,
-                color: Colors.teal,
-              ),
+        color: Colors.white70,
+        // 和下面的组件间隔1像素
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              child: Container(
+                alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    textInfo,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w900,
+                      color: Colors.teal,
+                    ),
+                  )),
+              onTap: () {
+                Navigator.pushNamed(context, routerName);
+              },
             ),
-            onTap: () {
-              Navigator.pushNamed(context, routerName);
-            },
-          ),
-          const Divider(height: 1, color: Colors.red,),
-        ],
-      )
-    );
+            const Divider(
+              height: 1,
+              color: Colors.black12,
+            ),
+          ],
+        ));
   }
 }
