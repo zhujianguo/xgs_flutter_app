9e259e3b204493d8f5083c653b687e6128ab50b2
jingle <zhujg@urevo.com>
2026-03-06 11:07:32 +0800

    Fix typo in MyApp.dart and clean up file

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'Route.dart';
 import 'package:get/get.dart';
@@ -21,7 +20,7 @@ class MyApp extends StatelessWidget {
 }
 /*
 title ： 在任务管理窗口中所显示的应用名字
-theme ： 应用各种 UI 所使用的主题颜色
+theme : 应用各种 UI 所使用的主题颜色
 color ： 应用的主要颜色值（primary color），也就是安卓任务管理窗口中所显示的应用颜色
 home ： 应用默认所显示的界面 Widget
 routes ： 应用的顶级导航表格，这个是多页面应用用来控制页面跳转的，类似于网页的网址
